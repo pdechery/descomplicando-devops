4a5705d654553a5f38f81ee1e9600c4ee07dd94b
Pierre <pierre.dechery@gmail.com>
2024-06-07 16:10:19 +0000

    Nova modif texto

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('MUNDO LUNAR TA CHEGANDO');
+  res.send('MUNDO LUNAR CHEGOU');
 });
 
 app.listen(PORT, HOST, () => {
